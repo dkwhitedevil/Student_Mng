6a5a9158392b2d0cf7da7a2b17b80cfb99f8589a
Dinesh Kumar <dineshkumar.26csa@licet.ac.in>
2025-04-20 23:40:23 +0530

    Update StudentController.java

--- src/main/java/com/example/demo/Controller/StudentController.java
+++ src/main/java/com/example/demo/Controller/StudentController.java
@@ -14,6 +14,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class StudentController
 {
+    //wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
     @Autowired
     private StudentService serv;
 
